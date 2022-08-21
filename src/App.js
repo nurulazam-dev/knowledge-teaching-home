0b48e2525c4afd506a981da35ee9da9c419d1575
mnaofficial <mna.official92@gmail.com>
2022-08-21 13:44:25 +0600

    home page create

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home';
+import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
   return (
     <div className='max-w-7xl mx-auto px-12'>
-      <h2>knowledge-teaching-home</h2>
+      <Routes>
+      <Route path="/" element={<Home />} />
+
+
+      <Route path="*" element={<NotFound />} />
+      </Routes>
       
     </div>
   );
