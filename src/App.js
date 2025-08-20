de3e39bad9e80bed9e5217b16a6dbcef2aa6e153
mnaofficialbd <mna.official92@gmail.com>
2025-08-20 10:09:59 +0600

    app update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import About from "./Pages/About";
-import Home from "./Pages/Home";
-import StudentLogin from "./Pages/Login/Login";
-import StudentRegister from "./Pages/Login/Register";
-import NotFound from "./Pages/NotFound";
-import Navbar from "./components/Shared/NavbarOld";
+import Layout from "./layout/Layout";
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/studentRegister" element={<StudentRegister />} />
-        <Route path="/studentLogin" element={<StudentLogin />} />
-
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
-  );
+  return <Layout />;
 }
 
 export default App;
