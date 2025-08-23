0b4389e30098abf25b694fb3b34983cb282c98c0
mnaofficialbd <mna.official92@gmail.com>
2025-08-23 13:27:47 +0600

    Applications page create

---
+++ src/Pages/Writings/ApplicationsPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ApplicationsPage = () => {
+  return (
+    <div>
+      <h1>Applications</h1>
+      <p>This is the applications page.</p>
+    </div>
+  );
+};
+
+export default ApplicationsPage;
