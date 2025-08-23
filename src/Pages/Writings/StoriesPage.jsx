1e53377587bae9af8f71670d455aeb7981ec4534
mnaofficialbd <mna.official92@gmail.com>
2025-08-23 13:37:00 +0600

    Stories page create

---
+++ src/Pages/Writings/StoriesPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const StoriesPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-6">Stories</h1>
+      <p className="text-gray-300 mb-12 max-w-2xl mx-auto">
+        Welcome to the Stories page! Here you can find a collection of engaging
+        stories to read and enjoy.
+      </p>
+    </div>
+  );
+};
+
+export default StoriesPage;
